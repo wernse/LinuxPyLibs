e3dbda61978e024c7626c26a88642960ad583bf1
Unknown <magic_Cards9@hotmail.com>
2017-10-12 19:41:31 +1300

    Added build ssm lambda function

--- LinuxLibs/functions/buildSSM/buildSSM.js
+++ LinuxLibs/functions/buildSSM/buildSSM.js
@@ -7,14 +7,43 @@ AWS.config.update({
 });
 let ssm = new AWS.SSM();
 
-let sendSSM = (apiInput, item, callback) => {
-    var command = `/home/ec2-user/bash/buildServerless.sh ${apiInput.bucket}/${apiInput.folderKey} ${apiInput.templateName} ${process.env.defaultPyName} ${process.env.snsTopic}`;
+module.exports = {
+    upload: upload
+}
+
+function upload(event, context, callback) {
+    let s3Event = event.Records[0].s3;
+    if (!s3Event || s3Event == null) {
+        console.log("Error no s3 event")
+        callback("Error no s3 Event");
+    }
+    console.log("s3Event", s3Event)
+    let s3RequirementsTxt = {
+        bucket: s3Event.bucket.name,
+        key: s3Event.object.key
+    }
+    async.waterfall([
+        async.apply(sendSSM, s3RequirementsTxt.bucket, s3RequirementsTxt.key)
+    ], (err, result) => {
+        if (err) {
+            console.log("PythonProcess err: ", err)
+            callback(err);
+            return;
+        }
+        console.log("PythonProcess result: ", result)
+        callback(null, "done")
+    })
+
+}
+
+let sendSSM = (bucket, folderKey, callback) => {
+    var command = `/home/ec2-user/buildLibs.sh ${bucket} ${folderKey}`;
 
     console.log("ssm command:", command);
     var params = {
         DocumentName: 'AWS-RunShellScript',
         /* required */
-        Comment: 'Building Serverless for: ' + apiInput.username + "method: " + apiInput.functionName,
+        Comment: 'Building Scriots for: ' + bucket + "method: " + folderKey,
         InstanceIds: [process.env.buildSSmInstance],
         OutputS3BucketName: 'serverlesslogs',
         OutputS3KeyPrefix: 'build/serverlesslogs',
@@ -33,6 +62,4 @@ let sendSSM = (apiInput, item, callback) => {
         console.log("sendCommand result: ", result)
         callback(null, "done")
     });
-
-
 }
